6709b3b646f8e453510cdcff24f7d91b314fa7fe
Zhecheng Zhao <zzhao84@sheffield.ac.uk>
2023-11-11 02:29:11 +0000

    pullRefresh demo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,6 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-text-android:1.5.4")
 
     testImplementation("junit:junit:4.13.2")
@@ -76,6 +75,9 @@ dependencies {
     debugImplementation("androidx.navigation:navigation-compose:$nav_version")
     val material3_version = "1.1.2"
     implementation("androidx.compose.material3:material3:$material3_version")
+    val material = "1.1.2"
+//    implementation("androidx.compose.material:material:$material")
+    implementation("androidx.compose.material:material")
     val swiperefreshlayout_version = "1.1.0"
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:$swiperefreshlayout_version")
 
--- app/src/main/java/cn/shef/msc5/todo/activity/DetailActivity.kt
+++ app/src/main/java/cn/shef/msc5/todo/activity/DetailActivity.kt
@@ -1,186 +1,188 @@
-//package cn.shef.msc5.todo.activity
-//
-//import android.os.Bundle
-//import android.util.Log
-//import androidx.activity.compose.setContent
-//import androidx.compose.foundation.background
-//import androidx.compose.foundation.layout.Arrangement
-//import androidx.compose.foundation.layout.Box
-//import androidx.compose.foundation.layout.PaddingValues
-//import androidx.compose.foundation.layout.Row
-//import androidx.compose.foundation.layout.fillMaxHeight
-//import androidx.compose.foundation.layout.fillMaxSize
-//import androidx.compose.foundation.layout.fillMaxWidth
-//import androidx.compose.foundation.layout.height
-//import androidx.compose.foundation.layout.padding
-//import androidx.compose.foundation.layout.width
-//import androidx.compose.foundation.lazy.LazyColumn
-//import androidx.compose.foundation.shape.RoundedCornerShape
-//import androidx.compose.material.icons.Icons
-//import androidx.compose.material.icons.filled.ArrowBack
-//import androidx.compose.material.icons.filled.Edit
-//import androidx.compose.material.icons.filled.FavoriteBorder
-//import androidx.compose.material3.Card
-//import androidx.compose.material3.ExperimentalMaterial3Api
-//import androidx.compose.material3.Icon
-//import androidx.compose.material3.IconButton
-//import androidx.compose.material3.MaterialTheme
-//import androidx.compose.material3.SnackbarDuration
-//import androidx.compose.material3.SnackbarHostState
-//import androidx.compose.material3.Surface
-//import androidx.compose.material3.Text
-//import androidx.compose.material3.TopAppBar
-//import androidx.compose.material3.TopAppBarDefaults
-//import androidx.compose.runtime.Composable
-//import androidx.compose.runtime.remember
-//import androidx.compose.runtime.rememberCoroutineScope
-//import androidx.compose.ui.Alignment
-//import androidx.compose.ui.Modifier
-//import androidx.compose.ui.graphics.Color
-//import androidx.compose.ui.platform.LocalContext
-//import androidx.compose.ui.unit.dp
-//import cn.shef.msc5.todo.base.BaseScaffold
-//import cn.shef.msc5.todo.base.BaseActivity
-//import kotlinx.coroutines.CoroutineScope
-//import kotlinx.coroutines.launch
-//
-///**
-// * @author Zhecheng Zhao
-// * @email zzhao84@sheffield.ac.uk
-// * @date Created in 02/11/2023 06:45
-// */
-//class DetailActivity : BaseActivity() {
-//
-//    val TAG = "DetailActivity"
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContent {
-//            MaterialTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    DetailScreen(this)
-//                }
-//            }
-//        }
-//    }
-//
-//    override fun onStart() {
-//        super.onStart()
-//        //TODO request permissions
-//        Log.d(TAG, "onStart: ")
-//    }
-//
-//}
-//
+package cn.shef.msc5.todo.activity
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import cn.shef.msc5.todo.base.BaseActivity
+import cn.shef.msc5.todo.model.viewmodel.DetailViewModel
+
+/**
+ * @author Zhecheng Zhao
+ * @email zzhao84@sheffield.ac.uk
+ * @date Created in 02/11/2023 06:45
+ */
+class DetailActivity : BaseActivity() {
+
+    val TAG = "DetailActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    DetailScreen()
+                }
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        //TODO request permissions
+        Log.d(TAG, "onStart: ")
+    }
+
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun DetailScreen(
+    modifier: Modifier = Modifier,
+    detailViewModel : DetailViewModel = viewModel()
+) {
+    //get context
+    val pullRefreshState = rememberPullRefreshState(
+        refreshing = detailViewModel.isRefreshing,
+        onRefresh = {
+            detailViewModel.getItems()
+        })
+//    val items by detailViewModel.items.collectAsStateWithLifecycle(initialValue = emptyList())
+    val items by detailViewModel.items.collectAsState(initial = emptyList())
+
+    // Box is used to place the pull to refresh indicator on top of the content
+    Box(modifier.pullRefresh(pullRefreshState)) {
+        // Your list or content
+        ItemsList(data = items, modifier = Modifier.fillMaxSize())
+        // the pull to refresh indicator
+        PullRefreshIndicator(
+            refreshing = detailViewModel.isRefreshing,
+            state = pullRefreshState,
+            Modifier.align(Alignment.TopCenter),
+            contentColor = MaterialTheme.colorScheme.primary,
+        )
+    }
+}
+
+@Composable
+fun ItemsList(data: List<String>, modifier: Modifier = Modifier) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(data) {
+            Card(modifier = Modifier.fillMaxWidth()) {
+                Text(text = it, modifier = Modifier.padding(16.dp))
+            }
+        }
+    }
+}
+
 //@OptIn(ExperimentalMaterial3Api::class)
 //@Composable
-//fun DetailScreen(detailActivity: DetailActivity) {
+//fun DetailScreen() {
 //    //get context
-//    val context = LocalContext.current
 //    val scope = rememberCoroutineScope()
 //    val snackbarHostState = remember { SnackbarHostState() }
 //    BaseScaffold(
-//        topBar = {
-//            TopAppBar(
-//                scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(),
-//                title = {
-//                    Text(text = "ToDos")
-//                },
-//                navigationIcon = {
-//                    IconButton(onClick = {
-////                        detailActivity.finish()
-//                        scope.launch {
-//                            Log.d("onClick", "onClick1")
-//                            snackbarHostState.showSnackbar("123",
-//                                duration = SnackbarDuration.Short)
-//                            Log.d("onClick", "onClick3")
-//                        }
-//                    }) {
-//                        Icon(
-//                            imageVector = Icons.Default.ArrowBack,
-//                            contentDescription = "Go back"
-//                        )
-//                    }
-//                },
-//                actions = {
-//                    IconButton(onClick = {
-//
-//                    }) {
-//                        Icon(
-//                            imageVector = Icons.Default.FavoriteBorder,
-//                            contentDescription = "Mark as favorite"
-//                        )
-//                    }
-//                    IconButton(onClick = {
-//
-//                    }) {
-//                        Icon(
-//                            imageVector = Icons.Default.Edit,
-//                            contentDescription = "Edit notes"
-//                        )
+//        showTopBar = false,
+//        floatingActionButton = {
+//            BaseFloatingActionBar(
+//                onClick = {
+//                    scope.launch {
+//                        snackbarHostState.showSnackbar("Snackbar")
 //                    }
 //                }
 //            )
 //        },
-//        content = {
-//            Content()
-//        }
-//    )
-//
-//
-//}
-//
-//@Composable
-//fun Content() {
-//    Box(
-//        modifier = Modifier
-//            .background(MaterialTheme.colorScheme.error)
-//            .fillMaxSize()
-//            .padding(horizontal = 15.dp),
+//        hostState = snackbarHostState,
 //    ) {
-//        LazyColumn(
-//            contentPadding = PaddingValues(vertical = 10.dp),
-//            verticalArrangement = Arrangement.spacedBy(10.dp)
-//        ) {
-////            ContentShimmer()
-//        }
-//    }
-//}
 //
-//@Composable
-//fun ContentShimmer(
-//    modifier: Modifier = Modifier
-//) {
-//    Card(
-//        modifier = modifier
-//            .fillMaxWidth()
-//            .height(100.dp),
-//        shape = RoundedCornerShape(8.dp),
-//    ) {
-//        Row(
-//            modifier = Modifier
-//                .padding(vertical = 10.dp, horizontal = 10.dp),
-//            horizontalArrangement = Arrangement.SpaceBetween
-//        ) {
-//            Box(
-//                contentAlignment = Alignment.CenterEnd,
-//                modifier = Modifier
-//                    .fillMaxHeight()
-//                    .width(24.dp)
-//                    .background(color = Color.LightGray)
-//            ) {
-//                Box(
-//                    Modifier
-//                        .fillMaxHeight()
-//                        .width(24.dp)
-//                        .background(color = Color.LightGray)
-//                ) {
-//                }
-//            }
-//        }
 //    }
+//
 //}
+
+@Composable
+fun Content() {
+    Box(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.error)
+            .fillMaxSize()
+            .padding(horizontal = 15.dp),
+    ) {
+        LazyColumn(
+            contentPadding = PaddingValues(vertical = 10.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+//            ContentShimmer()
+        }
+    }
+}
+
+@Composable
+fun ContentShimmer(
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(100.dp),
+        shape = RoundedCornerShape(8.dp),
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(vertical = 10.dp, horizontal = 10.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Box(
+                contentAlignment = Alignment.CenterEnd,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(24.dp)
+                    .background(color = Color.LightGray)
+            ) {
+                Box(
+                    Modifier
+                        .fillMaxHeight()
+                        .width(24.dp)
+                        .background(color = Color.LightGray)
+                ) {
+                }
+            }
+        }
+    }
+}
